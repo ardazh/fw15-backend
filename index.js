ebd19c3abff0e1aa41dae2e746c8d5096fe9cb24
ardazh <127732352+ardazh@users.noreply.github.com>
2023-04-05 16:52:40 +0700

    new repo backend

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config({
+    path: ".env"
+})
+
+const express = require("express")
+
+const app = express()
+app.use(express.urlencoded({extended: false}))
+
+app.use("/", require("./src/routers"))
+
+app.get("/", (request, response) => {
+    return response.json({
+        success: true,
+        message: "Backend is running well"
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, ()=>{
+    console.log(`App running on port ${PORT}`)
+})
