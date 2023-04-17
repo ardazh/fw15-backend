dad7275dc29aef02414e027aad9d8bf5ab0634af
ardazh <127732352+ardazh@users.noreply.github.com>
2023-04-17 16:08:19 +0700

    update filter file(JPEG)

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => {
         const explode = file.originalname.split(".").length
         const ext = file.originalname.split(".")[explode - 1]
-        const filename = (new Date().getTime()).toString() + "." + ext
+        const filename = new Date().getTime().toString() + "." + ext
         cb(null, filename)
     }
 })
@@ -16,13 +16,26 @@ const limits = {
     fileSize: 1 * 1024 * 1024
 }
 
-const upload = multer({storage, limits})
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype !== "image/jpeg"){
+        cb(Error("fileformat_error"))
+    }
+    cb(null, true)
+}
+
+const upload= multer({storage, limits, fileFilter})
 
 const uploadMiddleware =(field) => {
     const uploadField = upload.single(field)
-    return (request, response, next ) => {
+    return (request, response, next) => {
         uploadField(request, response, (err) => {
             if(err){
+                if(err.message === "fileformat_error"){
+                    return response.status(400).json({
+                        success: false,
+                        message: "File format invalid"
+                    })
+                }
                 return response.status(400).json({
                     success: false,
                     message: "File too large"
@@ -33,4 +46,4 @@ const uploadMiddleware =(field) => {
     }
 }
 
-module .exports = uploadMiddleware
+module.exports = uploadMiddleware
